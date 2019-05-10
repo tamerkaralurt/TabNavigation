2dcfdf198a149b3ecee20ff0bfab496013b0b215
Tamer <tamerkaralurt@gmail.com>
2019-05-10 04:17:35 +0300

    Tab Bar üzerine icon ve yazı eklendi

--- src/Router.js
+++ src/Router.js
@@ -2,19 +2,33 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {createBottomTabNavigator, createAppContainer} from "react-navigation";
 
+import Icon from 'react-native-vector-icons/Ionicons';
+
 import HomeScreen from './screens/Home';
 import ContactScreen from './screens/Contact';
 import SettingsScreen from './screens/Settings';
 
 const TabNavigator = createBottomTabNavigator({
     Home: {
-        screen: HomeScreen
+        screen: HomeScreen,
+        navigationOptions: {
+            tabBarLabel: 'Anasayfa',
+            tabBarIcon: ({tintColor}) => (<Icon name="ios-home" size={22}/>)
+        }
     },
     Contact: {
-        screen: ContactScreen
+        screen: ContactScreen,
+        navigationOptions: {
+            tabBarLabel: 'İletişim',
+            tabBarIcon: ({tintColor}) => (<Icon name="ios-contact" size={22}/>)
+        }
     },
     Settings: {
-        screen: SettingsScreen
+        screen: SettingsScreen,
+        navigationOptions: {
+            tabBarLabel: 'Ayarlar',
+            tabBarIcon: ({tintColor}) => (<Icon name="ios-settings" size={22}/>)
+        }
     }
 });
 
