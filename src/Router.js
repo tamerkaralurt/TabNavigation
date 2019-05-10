37dece585b634d7da415800d498905ba584c434b
Tamer <tamerkaralurt@gmail.com>
2019-05-10 03:55:05 +0300

    add homescreen

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {createBottomTabNavigator, createAppContainer} from "react-navigation";
 
 export default class Router extends Component {
-  render() {
-    return (
-      <View></View>
-    );
-  }
+    render() {
+        return (
+            <View></View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({});
