fa31abfc14add318decb9a5bfeffd6fd20adc4b0
Tamer <tamerkaralurt@gmail.com>
2019-05-10 04:03:54 +0300

    router düzenlendi ve App.js e eklendi

--- src/Router.js
+++ src/Router.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {createBottomTabNavigator, createAppContainer} from "react-navigation";
 
-export default class Router extends Component {
-    render() {
-        return (
-            <View></View>
-        );
-    }
-}
+import HomeScreen from './screens/Home';
+import ContactScreen from './screens/Contact';
+import SettingsScreen from './screens/Settings';
 
-const styles = StyleSheet.create({});
+const TabNavigator = createBottomTabNavigator({
+    Home: {
+        screen: HomeScreen
+    },
+    Contact: {
+        screen: ContactScreen
+    },
+    Settings: {
+        screen: SettingsScreen
+    }
+});
+
+export default createAppContainer(TabNavigator);
