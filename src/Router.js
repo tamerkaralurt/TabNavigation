07a4c04b054ac6316e91287cafe50a021fc6f7aa
Tamer <tamerkaralurt@gmail.com>
2019-05-10 03:50:38 +0300

    add router.js

---
+++ src/Router.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Router extends Component {
+  render() {
+    return (
+      <View></View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
